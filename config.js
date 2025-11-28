aad10f65b2b7b5ada4db5ede059af6c8afbea584
xyloflare <pro.dev.mohit@gmail.com>
2025-11-28 21:01:35 +0530

    test

--- config.js
+++ config.js
@@ -1,15 +1,2 @@
 module.exports = {
-  db: {
-    username: "root",
-    password: "",
-    database: "database",
-    host: "127.0.0.1",
-    dialect: "postgres",
-  },
-
-  Name: "Test for piney and changli",
-  token:
-    "MTIxMDY0MTU5NzYyMDgxMzk1Nw.GDJSB0.Tlyj6KKj5Yjx6kFnLITFB4sWuGxxTY7tpVOJko",
-  clientId: "1210641597620813957",
-  guildId: "1188471573078868008",
 };
